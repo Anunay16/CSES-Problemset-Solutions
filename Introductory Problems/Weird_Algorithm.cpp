27d35202a2f98b1657569cce5fbecb84d50442c6
Anunay Choudhury <64680716+Anunay16@users.noreply.github.com>
2021-11-12 13:48:25 +0530

    Create Weird_Algorithm.cpp

---
+++ Introductory Problems/Weird_Algorithm.cpp
@@ -0,0 +1,52 @@
+/* link to the problem: https://cses.fi/problemset/task/1068 */
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vs vector<string>
+#define rep(i, start, end) for(int i = start; i < end; i++)
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define all(x) (x).begin(), (x).end()
+ 
+void solve() {
+	int n;
+	cin >> n;
+	if (n == 1) {
+		cout << n;
+		return;
+	}
+	cout << n << " ";
+	while (n != 1) {
+		if (n % 2 == 0) {
+			n /= 2;
+		}
+		else {
+			n = n * 3 + 1;
+		}
+		cout << n << " ";
+	}
+}
+ 
+void FIO()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+signed main()
+{
+	fast;
+	FIO();
+	int T;
+	T = 1;
+	//cin >> T;
+	while (T--)
+		solve();
+	return 0;
+}
